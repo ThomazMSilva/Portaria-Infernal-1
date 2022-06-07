ab3ab07358969a15ee085c50a741e53e6a3c0839
Bruno Touzo <107005022+BrunoTouzo@users.noreply.github.com>
2022-06-07 17:33:48 -0300

    commit database 02
    correção da versão do bancoPortaria que estava no projeto java

--- src/scriptBanco/scriptBancoPortaria.sql
+++ src/scriptBanco/scriptBancoPortaria.sql
@@ -1,80 +1,75 @@
-create database banco_portaria_infernal;
+create database banco_portaria;
 
-use banco_portaria_infernal;
+use banco_portaria;
 
 create table tb_porteiro (
 	id_porteiro int auto_increment,
     nome_porteiro varchar(45) not null,
     senha_porteiro varchar(45) not null,
-    vida_porteiro smallint default 100,
-    mana_porteiro mediumint default 0,
-    dia_servico_porteiro smallint not null,
+    dia_servico_porteiro smallint not null default 1,
     
     primary key (id_porteiro)
 );
 
 create table tb_prestador(
 	id_prestador int auto_increment,
-    nome_prestador varchar(45) not null,
-    cpf_prestador varchar(11) not null,
-    contato_prestador varchar(10),
+    cpf_prestador bigint(11) not null unique,
+    nome_prestador varchar(50) not null,
+    contato_prestador varchar(30),
     
-    primary key (id_prestador, nome_prestador)
+    primary key (id_prestador, cpf_prestador)
 );
 
 create table tb_residente (
 	id_residente int auto_increment,
-    id_casa int,
+    id_casa int not null,
+    cpf_cliente bigint(11) unique,
     nome_residente varchar(50),
-    raca_residente varchar(15),
-    temperamento_residente varchar(10),
-    vivo_residente bool default true,
-    vida_residente smallint not null,
-    fora_residente bool default true,
+    contato_residente varchar(50),
     
     primary key(id_residente)
 );
 
-create table tb_evento (
-	id_evento int not null,
-    nome_evento varchar(20),
-    id_residente_evento int,
-    id_prestador_evento int,
-    taxa_perigo_evento tinyint,
-    
-    primary key (id_evento, nome_evento, id_residente_evento, id_prestador_evento),
-    foreign key (id_residente_evento) references tb_residente(id_residente),
-    foreign key (id_prestador_evento) references tb_prestador(id_prestador)
-);
-
 create table tb_servico(
 	id_servico int auto_increment,
+    id_prestador_servico int not null,
+    id_contratante_servico int not null,
     tipo_servico varchar(100),
-    contratante_servico varchar(50),
     data_agendada_servico date,
     data_realizada_servico date,
-    nome_prestador_servico varchar(45),
     
-    primary key (id_servico)
-    
+    primary key (id_servico, id_prestador_servico, id_contratante_servico),
+    constraint fk_id_prestador foreign key (id_prestador_servico)
+		references tb_prestador(id_prestador),
+	constraint fk_id_contratante foreign key (id_contratante_servico)
+		references tb_residente(id_residente)
 );
 
 create table tb_correspondencia (
 	id_correspondencia int auto_increment,
-    tipo_correspondencia varchar(30),
+    id_destinatario_correspondencia int not null,
+    tipo_correspondencia varchar(50),
     recebido_correspondencia datetime,
-    entregador_correspondencia varchar(50),
-    cpf_entregador_correspondencia varchar(11),
+    nome_entregador_correspondencia varchar(50),
+    cpf_entregador_correspondencia bigint(11),
     
-    primary key (id_correspondencia)
+    primary key (id_correspondencia,id_destinatario_correspondencia),
+    constraint fk_id_destinatario foreign key (id_destinatario_correspondencia)
+		references tb_residente(id_residente)
 );
 
--- adicionar tabela agenda
+create table tb_agenda(
+	id_agenda int auto_increment,
+    compromisso_agenda varchar(200),
+    
+    primary key (id_agenda)
+);
 
 create table tb_admin (
 	id_admin int auto_increment,
-    nome_admin varchar(50),
-    senha_admin varchar(50),
-    ativo_admin boolean,
-    primary key (id_admin)
+    cpf_admin bigint(11) not null unique,
+    nome_admin varchar(50) not null,
+    senha_admin varchar(50) not null,
+    
+    primary key (id_admin, cpf_admin)
 );
